b10b3385196d9cfc73556d5f3e96c7b238580a83
Carolina Thomaz <carol.com.jp@gmail.com>
2018-09-02 11:53:43 -0300

    Projeto criado

---
+++ src/main/java/com/carolinachang/blogmongodb/BlogMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.carolinachang.blogmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMongodbApplication.class, args);
+	}
+}
